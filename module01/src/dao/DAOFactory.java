b0496cf179d14e25ee105962a9164b2afe1c9745
indikaMax02 <indikamax02@gmail.com>
2022-05-20 21:55:55 +0530

    update dao layer

---
+++ module01/src/dao/DAOFactory.java
@@ -0,0 +1,35 @@
+package dao;
+
+import dao.custom.impl.CustomerDAOImpl;
+import jdk.nashorn.internal.runtime.regexp.JoniRegExp;
+
+public class DAOFactory {
+
+    private static DAOFactory daoFactory;
+
+    private DAOFactory(){
+
+    }
+    public static DAOFactory getDaoFactory(){
+        if (daoFactory==null){
+            daoFactory=new DAOFactory();
+        }
+        return daoFactory;
+    }
+
+    public enum DAOTypes{
+        CUSTOMER
+    }
+
+    public SuperDAO getDAO (DAOTypes types){
+        switch (types){
+
+            case CUSTOMER: return new CustomerDAOImpl();
+            default: return null;
+        }
+    }
+
+
+
+
+}
